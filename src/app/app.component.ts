3e27cb9d69f8f970471a6e2008c88280ee18b91e
Abel Borit <abelboritguitton@gmail.com>
2025-01-07 18:29:00 -0500

    [add] characters-container in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CharactersContainerComponent } from './components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CharactersContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
