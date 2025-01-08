ddd7de3ebd7fce3d6988d11f8b1ca5d7c4f10d07
Abel Borit <abelboritguitton@gmail.com>
2025-01-07 23:52:02 -0500

    [update] CharactersContainerComponent to use service with inject

--- src/app/components/characters-container/characters-container.component.ts
+++ src/app/components/characters-container/characters-container.component.ts
@@ -2,7 +2,11 @@
 - ChangeDetectionStrategy: Permite configurar cómo Angular detecta y actualiza los cambios en el componente.
 - Component: Decorador que marca la clase como un componente de Angular y permite configurar sus propiedades (como plantilla, estilos, etc.).
 */
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CharactersService } from '../../services/characters.service';
+import { Observable } from 'rxjs';
+import { CharacterInterface } from '../../interfaces';
 
 /*
 - selector: Define el nombre del selector del componente, que será usado como una etiqueta HTML para incluir este componente en plantillas de otros componentes.
@@ -11,21 +15,61 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 - templateUrl: Especifica el archivo que contiene el HTML del componente.
 - styleUrl: Define la ubicación del archivo de estilos (en este caso, SCSS) asociado al componente.
 - changeDetection: Configura la estrategia de detección de cambios del componente:
-
   - OnPush: Indica que Angular solo verificará los cambios en este componente si:
     - Las entradas (@Input) del componente cambian.
     - Se emiten eventos en el componente.
     - Se llama manualmente a markForCheck() o detectChanges().
-
     Esta estrategia mejora el rendimiento, especialmente en aplicaciones con muchos componentes, ya que reduce la frecuencia de las comprobaciones.
 */
 
 @Component({
   selector: 'app-characters-container',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './characters-container.component.html',
   styleUrl: './characters-container.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CharactersContainerComponent {}
+export class CharactersContainerComponent {
+  /* las propiedades de aquí tiene una relación con el -- ...component.html -- es por eso que se puede llamar o utilizar con doble llave {{}} y hacer referencia a las propiedades que coloquemos aquí */
+  // public title: string = 'Characters Container Component';
+
+  /* -- VERSIONES ANTES DE LA v18: para inyectar un servicio -- */
+  /* en versiones anteriores a la v18 para usar el servicio en un componente se tenía que inyectar el servicio que vayamos a usar en el constructor y luego crear una variable e inicializarla ya sea en el constructor o sino en otras directivas como en el ngOnInit y estar al tanto del ciclo de los componentes de Angular y que el servicio esté instanciado antes de que el componente se renderice, etc */
+  // charactersResponse$: Observable<CharacterInterface[]>; // se coloca con el $ porque como es algo asíncrono entonces es como un standard usarlo de esa forma
+
+  // constructor(private charactersService: CharactersService) {
+  //   this.charactersResponse$ = this.charactersService.getAllCharacters();
+  // }
+
+  // ngOnInit(): void {
+  //   /* lo que iría aquí se podría colocar en el constructor pero en el constructor es más que todo para inyección de dependencias y no tanto para cargar cosas del servicio, para eso usamos el OnInit y ngOnInit, igualmente hay varias formas de hacerlo y formas de mantener un orden */
+  // }
+
+  /* -- VERSIÓN v18: para inyectar un servicio -- */
+  /* nosotros podemos convertir un observable a una señal porque por mientras Angular trabaja fuertemente con los observables pero puede ser que en un futuro use plenamente las señales pero en este caso queremos usar las señales para poder optimizar el rendimiento aunque tranquilamente se podría seguir trabajando con los observables como la FORMA TRADICIONAL */
+  /* hacer tests con inject: https://www.youtube.com/watch?v=Tvsa4OMUGXs&ab_channel=RainerHahnekamp */
+  private charactersService = inject(CharactersService);
+
+  public characters$: Observable<CharacterInterface[]> =
+    this.charactersService.getAllCharacters();
+}
+
+/* ************************************************************************************************************************ */
+/* FORMA TRADICIONAL: sin señales y sin inject() */
+/*
+export class UserPageComponent implements OnInit {
+  user$: Observable<User>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.user$ = this.route.params.pipe(
+      switchMap(params => this.usersService.getUserById(params['id']))
+    );
+  }
+}
+*/
