8b0b1ff1ef719784a79fcd9445cfe0b92128fa96
Abel Borit <abelboritguitton@gmail.com>
2025-01-13 15:22:36 -0500

    [update] use "transformando el observable en una promesa"

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -3,9 +3,9 @@ import {
   Component,
   input,
   InputSignal,
-/*   OnInit,
+  OnInit,
   output,
-  OutputEmitterRef, */
+  OutputEmitterRef,
 } from '@angular/core';
 import { CharacterInterface } from '../../interfaces';
 import { JsonPipe, NgOptimizedImage } from '@angular/common';
@@ -18,7 +18,7 @@ import { JsonPipe, NgOptimizedImage } from '@angular/common';
   styleUrl: './character-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CharacterCardComponent /* implements OnInit */ {
+export class CharacterCardComponent implements OnInit {
   /* sintaxis antigua */
   /* antes para pasarle información a este componente desde el exterior, se tenía que colocar el decorador @Input() el cual se utiliza para la comunicación entre componentes que define una propiedad que puede ser enviada desde el componente padre hacia el componente hijo y se podía cambiar el nombre de la propidad por ejemplo usando @Input('otroNombre') */
   // @Input()
@@ -34,13 +34,13 @@ export class CharacterCardComponent /* implements OnInit */ {
     input<CharacterInterface>();
 
   /* nueva forma de utilizar el "output()" para mandar información desde el hijo hacia el padre */
-  // public readonly loaded: OutputEmitterRef<string> = output<string>();
+  public readonly loaded: OutputEmitterRef<string> = output<string>();
 
   /* en este caso se quiere que cuando el card reciba la información del character desde el padre entonces emitirá al padre la url de ese character */
-  // ngOnInit(): void {
-  //   this.loaded.emit(this.character().url);
+  ngOnInit(): void {
+    this.loaded.emit(this.character().url);
 
-  //   console.log(this.characterInfo() ?? 'No character info available');
-  //   console.log('this.characterInfo()', this.characterInfo());
-  // }
+    console.log(this.characterInfo() ?? 'No character info available');
+    console.log('this.characterInfo()', this.characterInfo());
+  }
 }
